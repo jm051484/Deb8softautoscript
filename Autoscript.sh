ef6e70493e36a0226274b51aa987d6a5d9dbae83
jm051484 <31314849+jm051484@users.noreply.github.com>
2019-03-11 16:44:48 +0800

    Update Autoscript.sh

--- Autoscript.sh
+++ Autoscript.sh
@@ -16,7 +16,7 @@ apt-get update
 apt-get install build-essential -y
 # Get the Softether Packages via wget and Save it into /root
 cd /root
-wget http://www.softether-download.com/files/softether/v4.10-9473-beta-2014.07.12-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.10-9473-beta-2014.07.12-linux-x64-64bit.tar.gz
+wget https://github.com/SoftEtherVPN/SoftEtherVPN_Stable/releases/download/v4.29-9680-rtm/softether-vpnserver-v4.29-9680-rtm-2019.02.28-linux-x64-64bit.tar.gz
 tar zxf softether-vpnserver-v4.10-9473-beta-2014.07.12-linux-x64-64bit.tar.gz
 # Making files, manual input needed
 cd vpnserver
@@ -32,7 +32,7 @@ chmod 700 vpncmd
 chmod 700 vpnserver
 # Downloading the Scripts for init.d and set Permission
 cd /root
-wget https://raw.githubusercontent.com/jm051484/Deb8softautoscript/master/vpnserver-debian.sh --no-check-certificate
+wget https://raw.githubusercontent.com/jm051484/Deb8softether/master/vpnserver-debian.sh --no-check-certificate
 mv vpnserver-debian.sh /etc/init.d/vpnserver
 cd /etc/init.d
 chmod 755 vpnserver
